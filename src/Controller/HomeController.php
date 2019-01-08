46e0fef22bc34c137268231c6b347866e8fc85fe
Gafarzadeh <sima.gafarzadeh@gmail.com>
2019-01-08 11:24:39 +0100

    Show last table

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\HostTableRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,13 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(HostTableRepository $repository)
     {
+        $table = $repository->getRandomTable();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'Best table',
+            'table' => $table,
         ]);
     }
 }
